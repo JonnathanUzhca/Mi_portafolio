0ff319563c2aa1025675da42258fd7e4a77bf495
Jonnathan <jonathan.uzhca0106@gmail.com>
2024-05-30 16:22:42 -0500

    version 01.0.0 modificacion de  haeder codigo escrito

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,6 +1,10 @@
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component,HostListener, ElementRef, OnInit, ViewChild, Renderer2, QueryList, AfterContentInit } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -50,6 +54,7 @@ export class HomeComponent  implements OnInit {
 
   ngOnInit(): void {
     this.onWindowScroll();
+   
     
   }
 
