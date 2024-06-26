73663c05e0731bb9a003749c2a110f7de1e02645
Jonnathan <jonathan.uzhca0106@gmail.com>
2024-06-26 02:23:11 -0500

    version 01.0.0 modifcacion de resposive

--- src/app/public/pages/about-me/about-me-routing.module.ts
+++ src/app/public/pages/about-me/about-me-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
+import { AboutMeComponent } from './about-me.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
+    path: "",
+    component: AboutMeComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomeRoutingModule { }
+export class AboutMeRoutingModule { }
